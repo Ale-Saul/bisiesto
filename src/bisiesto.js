40cda6d143455b35bf2fc1ab9f60acc271bd50b7
Alejandro <alejandro.mont.cesp@gmail.com>
2024-02-25 00:23:25 -0400

    se añadio la segunda regla de año bisiesto

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -2,6 +2,9 @@ function bisiesto(anio) {
   if (anio % 400 === 0) {
     return "Es bisiesto";
   }
+  else if (anio % 100 === 0 && anio % 400 !== 0) {
+    return "No es bisiesto";
+  }
 }
 
 export default bisiesto;
--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -4,4 +4,7 @@ describe('bisiesto', () => {
   it('deberia devolver "Es bisiesto" si el año es divisible por 400', () => {
     expect(bisiesto(1600)).toEqual("Es bisiesto");
   });
+    it('deberia devolver "No es bisiesto" si el año es divisible por 100 pero no por 400', () => {
+        expect(bisiesto(1700)).toEqual("No es bisiesto");
+    });
 });
